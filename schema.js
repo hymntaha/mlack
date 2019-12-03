b2b3b82776b7fbefae952c7715e36ff253598d6e
taha uygun <tahauygun@gmail.com>
2019-12-02 21:58:51 -0800

    adding main schema and fields

--- schema.js
+++ schema.js
@@ -1,5 +1,33 @@
 export default `
+  type Team {
+    ownder: User!
+    members: [User!]!
+    channels: [Channel!]!
+  }
+
+  type Channel {
+    id: Int!
+    name: String!
+    public: Boolean!
+    messages: [Messages]!
+  }  
+
+  type Message {
+    id: Int!
+    text: String!
+    user:User!
+    channel: Channel!
+  }
+
+  type User {
+    id: Int!
+    username: String!
+    email: String!
+    messages: Message!
+    teams: [Team!]!
+  }
+
   type Query {
     hi: String
   }
-`
+`;
