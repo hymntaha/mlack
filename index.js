14795dcca3042c5264e3ba5aa0364b200120a2f7
taha uygun <tahauygun@gmail.com>
2019-12-03 21:26:25 -0800

    merge resolvers

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
 import { makeExecutableSchema } from "graphql-tools";
-import { fileLoader, mergeTypes } from "merge-graphql-schemas";
+import { fileLoader, mergeTypes, mergeResolvers } from "merge-graphql-schemas";
 
 import models from "./models";
 
 const types = fileLoader(path.join(__dirname, "./schema"));
+const resolvers = mergeTypes(fileLoader(path.join(__dirname, "./resolvers")));
 const typeDefs = mergeTypes(types);
 
 export const schema = makeExecutableSchema({
